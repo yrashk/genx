8ae3e9f2529c4a2ed8d382d8dee16b7b7abcce06
Yurii Rashkovskii <yrashk@spawngrid.com>
2012-05-28 16:15:59 -0700

    Implements Supervisor.Behavior and GenX.Supervisor

---
+++ lib/supervisor.ex
@@ -0,0 +1,157 @@
+defmodule Supervisor.Behavior do
+  defmacro __using__(_) do
+    quote do
+      @behavior :supervisor
+    end
+  end
+end
+
+defmodule GenX.Supervisor do
+  use Supervisor.Behavior
+
+  defrecord Child, id: :undefined, start_func: :undefined, restart: :permanent,
+                   shutdown: 5000, type: :undefined, modules: []
+
+  defrecord Worker, id: :undefined, start_func: :undefined, restart: :permanent,
+                    shutdown: 5000, modules: []
+
+  defrecord Sup, id: :undefined, registered: false, restart_strategy: :undefined,
+                        max_restarts: {1,60}, children: [], restart: :permanent,
+                        shutdown: :infinity
+
+  defrecord OneForOne, id: :undefined, registered: false,
+                        max_restarts: {1,60}, children: [], restart: :permanent,
+                        shutdown: :infinity
+
+  defrecord OneForAll, id: :undefined, registered: false,
+                        max_restarts: {1,60}, children: [], restart: :permanent,
+                        shutdown: :infinity
+  defrecord RestForOne, id: :undefined, registered: false,
+                        max_restarts: {1,60}, children: [], restart: :permanent,
+                        shutdown: :infinity
+  defrecord SimpleOneForOne, id: :undefined, registered: false,
+                        max_restarts: {1,60}, children: [], restart: :permanent,
+                        shutdown: :infinity
+
+  defprotocol Supervision do
+    @only [Record]
+    def supervise(s)
+  end
+
+  defprotocol Supervisor do
+    @only [Record]
+    def supervisor(s)
+    def cast(s)
+  end
+
+  defimpl Supervision, for: Child do
+    def supervise(Child[id: id, start_func: :undefined]=c) when id !== :undefined and is_atom(id) do
+        supervise(c.start_func({id, :start_link, []}))
+    end
+    def supervise(s) do
+      spec = {s.id, s.start_func, s.restart, s.shutdown, s.type, s.modules}
+      :ok = :supervisor.check_childspecs([spec])
+      spec
+    end
+  end
+
+  defimpl Supervision, for: Worker do    
+    def supervise(s), do: GenX.Supervisor.supervise cast(s)
+    defp cast(s) do
+        Child.new(id: s.id, start_func: s.start_func,
+                  restart: s.restart, shutdown: s.shutdown, type: :worker, modules: s.modules)
+    end
+  end
+
+  defimpl Supervision, for: Sup do
+    def supervise(s) do
+      GenX.Supervisor.supervise Child.new(id: s.id, start_func: {GenX.Supervisor, :start_link, [s]}, 
+                                          restart: s.restart, shutdown: s.shutdown, type: :supervisor, modules: [:dynamic])
+    end
+  end
+
+  defimpl Supervisor, for: Sup do
+
+    def supervisor(s) do
+      {max_r, max_t} = s.max_restarts
+      {{s.restart_strategy, max_r, max_t}, lc child in s.children, do: GenX.Supervisor.supervise child}
+    end
+
+    def cast(s), do: s
+
+  end
+
+  # one_for_one
+  defimpl Supervisor, for: OneForOne do    
+    def supervisor(s), do: GenX.Supervisor.supervisor cast(s)
+    def cast(s) do
+        Sup.new(id: s.id, registered: s.registered, restart_strategy: :one_for_one,
+                max_restarts: s.max_restarts, children: s.children, restart: s.restart,
+                shutdown: s.shutdown)
+    end
+  end
+
+  defimpl Supervision, for: OneForOne do    
+    def supervise(s), do: GenX.Supervisor.supervise GenX.Supervisor.cast(s)
+  end
+
+  # one_for_all
+  defimpl Supervisor, for: OneForAll do    
+    def supervisor(s), do: GenX.Supervisor.supervisor cast(s)
+    def cast(s) do
+        Sup.new(id: s.id, registered: s.registered, restart_strategy: :one_for_all,
+                max_restarts: s.max_restarts, children: s.children, restart: s.restart,
+                shutdown: s.shutdown)
+    end
+  end
+
+  defimpl Supervision, for: OneForAll do    
+    def supervise(s), do: GenX.Supervisor.supervise GenX.Supervisor.cast(s)
+  end
+
+  # rest_for_one
+  defimpl Supervisor, for: RestForOne do    
+    def supervisor(s), do: GenX.Supervisor.supervisor cast(s)
+    def cast(s) do
+        Sup.new(id: s.id, registered: s.registered, restart_strategy: :rest_for_one,
+                max_restarts: s.max_restarts, children: s.children, restart: s.restart,
+                shutdown: s.shutdown)
+    end
+  end
+
+  defimpl Supervision, for: RestForOne do    
+    def supervise(s), do: GenX.Supervisor.supervise GenX.Supervisor.cast(s)
+  end
+
+  # simple_one_for_one
+  defimpl Supervisor, for: SimpleOneForOne do    
+    def supervisor(s), do: GenX.Supervisor.supervisor cast(s)
+    def cast(s) do
+        Sup.new(id: s.id, registered: s.registered, restart_strategy: :simple_one_for_one,
+                max_restarts: s.max_restarts, children: s.children, restart: s.restart,
+                shutdown: s.shutdown)
+    end
+  end
+
+  defimpl Supervision, for: SimpleOneForOne do    
+    def supervise(s), do: GenX.Supervisor.supervise GenX.Supervisor.cast(s)
+  end
+
+
+  defdelegate [supervise: 1], to: Supervision
+  defdelegate [supervisor: 1, cast: 1], to: Supervisor
+
+  def start_link(sup) do
+    case sup.registered do
+      false ->
+        :supervisor.start_link(__MODULE__, supervisor(sup))
+      name ->
+        :supervisor.start_link({:local, name}, __MODULE__, supervisor(sup))
+    end
+  end
+
+  def init(sup) do
+    {:ok, sup}
+  end
+
+end
---
+++ test/supervisor_test.exs
@@ -0,0 +1,25 @@
+defmodule GenX.Supervisor.Sample do
+  alias GenX.Supervisor, as: Sup
+
+  def start_link do
+      tree = Sup.OneForOne.new(id: OFO,
+                               children: [
+                                  Sup.OneForOne.new(id: OFO.Registered, registered: OFO.Registered,
+                                                    children: [])
+                               ])
+      Sup.start_link tree
+  end
+
+end
+
+defmodule GenX.Supervisor.Test do
+   use ExUnit.Case
+   alias GenX.Supervisor.Sample, as: S
+
+   test "start" do
+       {:ok, pid} = S.start_link
+       assert [{OFO.Registered, pid_ofor, :supervisor, _}] = :supervisor.which_children(pid)
+       assert :supervisor.which_children(pid_ofor) == []
+       assert Process.whereis(OFO.Registered) == pid_ofor
+   end
+end
