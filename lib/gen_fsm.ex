e532d594cf517adc85ccd5a094addc6a9ab4baa4
Yurii Rashkovskii <yrashk@gmail.com>
2012-05-28 00:51:31 -0700

    Adds GenFSM.Behavior and GenX.GenFSM

---
+++ lib/gen_fsm.ex
@@ -0,0 +1,67 @@
+defmodule GenFsm.Behavior do
+ defmacro __using__(_, _) do
+    quote do
+      @behavior :gen_fsm
+
+      def handle_event(_event, state_name, state) do
+        { :next_state, state_name, state }
+      end
+
+      def handle_sync_event(_event, _from, state_name, state) do
+        { :next_state, state_name, state }
+      end
+
+      def handle_info(_msg, state_name, state) do
+        { :next_state, state_name, state }
+      end
+
+      def terminate(reason, state_name, state) do
+        :error_logger.error_report('#{inspect __MODULE__} crashed:\n#{inspect reason}')
+        :error_logger.error_report('#{inspect __MODULE__} snapshot:\n#{inspect state_name}/#{inspect state} ')
+        :ok
+      end
+
+      def code_change(_old, state_name, state, _extra) do
+        { :ok, state_name, state }
+      end
+
+      defoverridable [handle_event: 3, handle_sync_event: 4, handle_info: 3, terminate: 3, code_change: 4]
+    end
+ end
+end
+defmodule GenX.GenFsm do
+  import GenX.Gen
+
+   defp _defevent({:"/",_,[{state_name,_,_},event]}, options, body) do
+        _defevent(event, Keyword.merge(options, [in: state_name]), body)
+   end
+   defp _defevent(event, options, body) do
+     state_name = options[:in]
+     if (not (options[:all_states]||false) and state_name == nil), do: throw(:badarg)
+     {event_sender, handler, handle} = case ({options[:all_states]||false,options[:sync]||false}) do
+               {false,false} -> {:send_event, state_name, []}
+               {false,true} -> {:sync_send_event, state_name, [:from]}
+               {true, false} -> {:send_all_state_event, :handle_event, [:state_name]}
+               {true, true} -> {:sync_send_all_state_event, :handle_sync_event, [:from, :state_name]}
+             end
+    defhandler(handler, {:gen_fsm, event_sender}, event, Keyword.from_enum(options ++ body), [handle: handle])
+   end
+
+   defmacro defevent(event, options, body) do
+     _defevent(event, options, body)
+   end
+
+   defmacro defevent(event, options) do
+    _defevent(event, options, [do: options[:do]])
+   end
+
+   defmacro definfo(info, options, body) do
+    defhandler(:handle_info, {:erlang, :send}, info, Keyword.from_enum(options ++ body), [handle: [:state_name]])
+   end
+
+   defmacro definfo(info, body) do
+    defhandler(:handle_info, {:erlang, :send}, info, body, [handle: [:state_name]])
+   end
+
+
+end
---
+++ test/gen_fsm_test.exs
@@ -0,0 +1,149 @@
+defmodule GenX.GenFsm.Sample do
+ use GenFsm.Behavior
+ import GenX.GenFsm
+
+ def init(_), do: {:ok, :testing, nil}
+
+ defevent testing/event, do: {:next_state, :testing, :event}
+ defevent testing/event(a), do: {:next_state, :testing, a}
+ defevent testing/event(a,b), do: {:next_state, :testing, {a,b}}
+ defevent testing/event(a,b,c), do: {:next_state, :testing, {a,b,c}}
+
+ defevent testing/private_event, export: false, do: {:next_state, :testing, :private_event}
+
+ defevent testing/named_event, export: SampleFSM, do: {:next_state, :testing, :named_event}
+
+ defevent testing/sync_event, sync: true, do: {:reply, :sync_event, :testing, :sync_event}
+
+ defevent testing/sync_all_states_event, sync: true, all_states: true, do: {:reply, :sync_all_states_event, :testing, :sync_all_states_event}
+
+ defevent testing/all_states_event, all_states: true, do: {:next_state, :testing, :all_states_event}
+
+ definfo info, state_name: state_name, do: {:next_state, state_name, :info}
+ definfo info(a), state_name: state_name, do: {:next_state, state_name, a}
+ definfo info(a, b), state_name: state_name, do: {:next_state, state_name, {a,b}}
+ definfo info(a, b, c), state_name: state_name, do: {:next_state, state_name, {a,b,c}}
+
+ definfo private_info, state_name: state_name, export: false, do: {:next_state, state_name, :private_info}
+ definfo named_info, state_name: state_name, export: SampleFSM, do: {:next_state, state_name, :named_info}
+
+ defevent get_state, state_name: state_name, state: state,
+                     all_states: true, sync: true do
+    {:reply, state, state_name, state} 
+ end
+
+ # Suppress error logging
+ def terminate(_, _), do: :ok
+end
+
+
+defmodule GenX.GenFsm.Test do    
+  refer GenX.GenFsm.Sample, as: S
+  refer :gen_fsm, as: FSM
+  use ExUnit.Case
+
+  test "regular event with no arguments" do
+      {:ok, pid} = FSM.start_link S, [], []
+      S.event(pid)
+      assert S.get_state(pid) == :event
+  end
+
+  test "regular event with one argument" do
+      {:ok, pid} = FSM.start_link S, [], []
+      S.event(pid, 1)
+      assert S.get_state(pid) == 1
+  end
+
+
+  test "regular event with multiple arguments" do
+      {:ok, pid} = FSM.start_link S, [], []
+      S.event(pid, 1, 2)
+      assert S.get_state(pid) == {1,2}
+
+      S.event(pid, 1, 2, 3)
+      assert S.get_state(pid) == {1,2,3}
+
+  end
+
+  test "private event" do
+      {:ok, pid} = FSM.start_link S, [], []
+
+      assert not :erlang.function_exported(S, :private_event, 1)
+      FSM.send_event pid, :private_event
+
+      assert S.get_state(pid) == :private_event
+  end
+
+  test "named event" do
+      {:ok, _pid} = FSM.start_link({:local, SampleFSM}, S, [], []) 
+
+      assert S.named_event == :ok
+      assert S.get_state(SampleFSM) == :named_event
+      Process.unregister SampleFSM
+  end
+
+  test "sync event" do
+      {:ok, pid} = FSM.start_link S, [], []
+      assert S.sync_event(pid) == :sync_event
+  end
+
+  test "sync + all states event" do
+      {:ok, pid} = FSM.start_link S, [], []
+      assert S.sync_all_states_event(pid) == :sync_all_states_event
+  end
+
+  test "all states event" do
+      {:ok, pid} = FSM.start_link S, [], []
+      S.all_states_event(pid)
+      assert S.get_state(pid) == :all_states_event
+  end
+
+
+  test "regular info call with no arguments" do
+      {:ok, pid} = FSM.start_link S, [], []
+      S.info(pid)
+      assert S.get_state(pid) == :info
+  end
+
+  test "regular info call with one argument" do
+      {:ok, pid} = FSM.start_link S, [], []
+
+      S.info(pid, 1)
+      assert S.get_state(pid) == 1
+
+      S.info(pid, 1, 2)
+      assert S.get_state(pid) == {1,2}
+
+      S.info(pid, 1, 2, 3)
+      assert S.get_state(pid) == {1,2,3}
+  end
+
+  test "regular info call with multiple arguments" do
+      {:ok, pid} = FSM.start_link S, [], []
+
+      S.info(pid, 1, 2)
+      assert S.get_state(pid) == {1,2}
+
+      S.info(pid, 1, 2, 3)
+      assert S.get_state(pid) == {1,2,3}
+
+  end
+
+  test "private info" do
+      {:ok, pid} = FSM.start_link S, [], []
+      assert not :erlang.function_exported(S, :private_info, 1)
+      pid <- :private_info
+      assert S.get_state(pid) == :private_info
+  end
+
+  test "named info" do
+      {:ok, _pid} = FSM.start_link {:local, SampleFSM}, S, [], []
+
+      assert S.named_info == :named_info
+      assert S.get_state(SampleFSM) == :named_info
+      Process.unregister SampleFSM
+  end
+
+
+
+end
