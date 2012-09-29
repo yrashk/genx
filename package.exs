f1a897ecb2b3df73138f19081d5280c613aa08f0
Yurii Rashkovskii <yrashk@spawngrid.com>
2012-09-28 21:12:15 -0700

    Add expm package

---
+++ package.exs
@@ -0,0 +1,5 @@
+Expm.Package.new(name: "genx", description: "Elixir-style library for most important OTP functionality",
+                 version: "0.1", keywords: ["otp"], 
+                 maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
+                 contributors: [[name: "ILYA Khlopotov", email: "ilya@spawngrid.com"]],
+                 repositories: [[github: "yrashk/genx", tag: "0.1"]])
