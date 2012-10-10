c70392ec36212138227bea7aeb00325ef9a92a7d
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-10 13:49:35 -0700

    Add keywords to the expm package

--- package.exs
+++ package.exs
@@ -1,5 +1,5 @@
 Expm.Package.new(name: "genx", description: "Elixir-style library for most important OTP functionality",
-                 version: "0.1", keywords: ["otp"], 
+                 version: "0.1", keywords: ["otp","gen_server","gen_event","gen_fsm","supervisor","ct"], 
                  maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
                  contributors: [[name: "ILYA Khlopotov", email: "ilya@spawngrid.com"]],
                  repositories: [[github: "yrashk/genx", tag: "0.1"]])
--- package.head.exs
+++ package.head.exs
@@ -1,5 +1,5 @@
 Expm.Package.new(name: "genx", description: "Elixir-style library for most important OTP functionality",
-                 version: :head, keywords: ["otp"], 
+                 version: :head, keywords: ["otp","gen_server","gen_event","gen_fsm","supervisor","ct"], 
                  maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
                  contributors: [[name: "ILYA Khlopotov", email: "ilya@spawngrid.com"]],
                  repositories: [[github: "yrashk/genx"]])
